512969860b9529e4d1ca40ad2ee2b3b867f665d0
Frederik Wille <frederik@wille.hamburg>
2017-05-23 15:49:39 +0200

    add osx docker machine on boot script
    adds a script which installs a plist into user's Library that starts the
    default docker-machine after boot of the system
    
    zshrc already evals docker-machine env to use the docker-machine

---
+++ osx-docker-machine-boot.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+curl -sL http://git.io/vsk46 | \
+    sed -e "s?{{docker-machine}}?$(which docker-machine)?" \
+        -e "s?{{user-path}}?$(echo $PATH)?" \
+    >~/Library/LaunchAgents/com.docker.machine.default.plist && \
+    launchctl load ~/Library/LaunchAgents/com.docker.machine.default.plist
