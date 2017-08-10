8b7d38bdf15aea3f11145802b3d3bceb306468f8
Frederik Wille <frederik@wille.hamburg>
2017-08-10 14:29:12 +0200

    [arch] update bottom status bar
    * use pulseaudio bar module instead of alsa
    * show seperated Buildin-Stereo and HDMI volumes

--- tag-arch/i3/i3pystatus-bottom.py
+++ tag-arch/i3/i3pystatus-bottom.py
@@ -12,6 +12,7 @@ green  = "#9BEE47"
 red    = "#C75646"
 yellow = "#FFE377"
 white  = "#F7F7F7"
+grey = "#AAAAAA"
 
 # Displays clock like this:
 # Tue 30 Jul 11:59:46 PM KW31
@@ -35,10 +36,24 @@ status.register("backlight",
         backlight="intel_backlight",
         interval=1)
 
-status.register("alsa",
-        format=" {volume}%",
-        format_muted=" muted",
-        card = 1
+#status.register("alsa",
+#        format=" {volume}%",
+#        format_muted=" muted",
+#        card = 1
+#        )
+
+status.register("pulseaudio",
+        format="{selected} {volume}%",
+        format_muted="{selected} muted",
+        color_muted=grey,
+        sink="alsa_output.pci-0000_00_1b.0.analog-stereo"
+        )
+
+status.register("pulseaudio",
+        format="{selected} {volume}% HDMI",
+        format_muted="{selected} muted HDMI",
+        color_muted=grey,
+        sink="alsa_output.pci-0000_00_03.0.hdmi-stereo"
         )
 
 status.register("battery",
